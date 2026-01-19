d844776de3638fe96813a6587cbf611c74d9a106
pyry <pyryjunnila@gmail.com>
2026-01-19 03:56:52 +0200

    config.js push to git

---
+++ väinön sivu/config.js
@@ -0,0 +1,10 @@
+window.EMAIL_CONFIG = {
+    publicKey: "slfLpRs9ANctKJnZS",
+    serviceId: "service_7w0yt4e",
+    template1: "template_g8d40sv",
+    template2: "template_8ajfoh9"
+};
+
+window.RECAPTCHA_CONFIG = {
+    siteKey: "6LfjBE8sAAAAAMbErDke0XrpRTSKGW6uxiyXcHkL"  // This is your public HTML key
+};
